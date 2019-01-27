8b81fe356e7c5de30c7f07d3db79d38af3c666be
Audrey K Tinkey <audrey-thoma@uiowa.edu>
2019-01-26 22:33:59 -0600

    adding commenting to hw1.2.py

--- hw1.2.py
+++ hw1.2.py
@@ -1,15 +1,13 @@
-import pandas as pd
+import pandas as pd                                                     # import pandas for data reading and manipulating libraries
 
-divorce = pd.read_excel('data/state-divorce-rates-90-95-99-17.xlsx',
-                        skiprows=5,
-                        na_values='---',
-                        skipfooter=7,
+divorce = pd.read_excel('data/state-divorce-rates-90-95-99-17.xlsx',    # read from this file name
+                        skiprows=5,                                     # skip first 5 rows of metadata/description
+                        na_values='---',                                # defines null values in file
+                        skipfooter=7,                                   # skip last 7 rows of metadata/description
                         )
 
-# divorce = divorce.set_index()
-divorce.rename(columns={'Unnamed: 0': 'State'}, inplace=True )
-divorce.dropna(how='all', inplace=True)
-divorce.to_excel(excel_writer='data/divorce_clean.xls',
-                 sheet_name='divorce rate',
-                 na_rep='null',
-                 index=False)
+divorce.rename(columns={'Unnamed: 0': 'State'}, inplace=True)           # renames cell A1 to "State"
+divorce.dropna(how='all', inplace=True)                                 # replaces all values that have been designated as null
+divorce.to_excel(excel_writer='data/divorce_clean.xls',                 # output changes to new excel file
+                 sheet_name='divorce rate',                             # sheet name
+                 na_rep='null')                                         # defines how we want to display null values
