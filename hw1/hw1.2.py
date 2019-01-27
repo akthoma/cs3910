42bf600830391e0a95b6756953711f9be491969c
Audrey K Tinkey <audrey-thoma@uiowa.edu>
2019-01-26 22:47:10 -0600

    elegant-izing code and comments

--- hw1/hw1.2.py
+++ hw1/hw1.2.py
@@ -3,10 +3,9 @@ import pandas as pd                                                     # import
 divorce = pd.read_excel('data/state-divorce-rates-90-95-99-17.xlsx',    # read from this file name
                         skiprows=5,                                     # skip first 5 rows of metadata/description
                         na_values='---',                                # defines null values in file
-                        skipfooter=7,                                   # skip last 7 rows of metadata/description
-                        )
+                        skipfooter=7)                                   # skip last 7 rows of metadata/description
 
-divorce.rename(columns={'Unnamed: 0': 'State'}, inplace=True)           # renames cell A1 to "State"
+divorce.rename(columns={'Unnamed: 0': 'State'}, inplace=True)           # renames missing column header to "State"
 divorce.dropna(how='all', inplace=True)                                 # replaces all values that have been designated as null
 divorce.to_excel(excel_writer='data/divorce_clean.xls',                 # output changes to new excel file
                  sheet_name='divorce rate',                             # sheet name
