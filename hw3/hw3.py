50dd62cb29d152909d28023df67fb490512ca61a
Audrey Thoma <audrey-thoma@uiowa.edu>
2019-02-21 13:23:34 -0600

    adding loop for analyzing more files

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,10 +1,18 @@
 import pandas as pd
 
+a = 'h08b.xls'
+b = 'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv'
+c = 'state-divorce-rates-90-95-99-17.xlsx'
+d = 'state-marriage-rates-90-95-99-17.xlsx'
+e = 'tab-a-1.xls'
+g = 'Unemployment rate by state 2000-2017.csv'
+h = 'CrimeTrendsInOneVar.csv'
 
-def csv(f, sr, sf, na, ic, h):
+
+def csv(f, sr, sf, na, ic, head):
     df = pd.read_csv('data/{0}'.format(f),
                      skiprows=sr,
-                     header=h,
+                     header=head,
                      na_values=na,
                      skipfooter=sf,
                      index_col=ic
@@ -12,10 +20,10 @@ def csv(f, sr, sf, na, ic, h):
     return df
 
 
-def excel(f, sr, sf, na, ic, h):
+def excel(f, sr, sf, na, ic, head):
     df = pd.read_excel('data/{0}'.format(f),
                        skiprows=sr,
-                       header=h,
+                       header=head,
                        na_values=na,
                        skipfooter=sf,
                        index_col=ic
@@ -31,119 +39,134 @@ def merger(f):
     merged.to_csv('data/{0}.csv'.format(f), index=False)
 
 
-a = 'h08b.xls'
-b = 'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv'
-c = 'state-divorce-rates-90-95-99-17.xlsx'
-d = 'state-marriage-rates-90-95-99-17.xlsx'
-e = 'tab-a-1.xls'
-g = 'Unemployment rate by state 2000-2017.csv'
-h = 'CrimeTrendsInOneVar.csv'
+def cleaner(f, filenames):
+    if f not in filenames:
+        sr = input('How many lines need to be skipped in the header? ')
+        sf = input('How many lines need to be skipped in the footer? ')
+        na = input('How are null values represented in this dataset? ')
+        ic = [int(x) for x in input('Enter which column(s) will be used for the index'
+                                    ' (separate with only spaces, NOT commas): ').split()]
+        head = [int(x) for x in input('Enter which column(s) will be used for the header'
+                                      ' (separate with only spaces, NOT commas): ').split()]
+    elif f == a:
+        sr = 4
+        head = [0, 1]
+        sf = 1
+        ic = 0
+        na = 'null'
+    elif f == b or f == g:
+        sr = 6
+        na = 'N/A'
+        ic = 0
+        head = 0
+        sf = 0
+    elif f == c:
+        sr = 5
+        na = '---'
+        sf = 7
+        ic = 0
+        head = 0
+    elif f == d:
+        sr = 5
+        na = '---'
+        sf = 8
+        ic = 0
+        head = 0
+    elif f == e:
+        sr = 4
+        na = '(NA)'
+        ic = 0
+        head = [0, 1, 2]
+        sf = 12
+    elif f == h:
+        merger(f)
+        sr = 4
+        head = 0
+        sf = 1
+        ic = 0
+        na = 'null'
+
+    if '.csv' in f:
+        data = csv(f, sr, sf, na, ic, head)
+    elif '.xls' in f:
+        data = excel(f, sr, sf, na, ic, head)
+    else:
+        print('Data in unsupported format. Please convert file to .csv or .xls/.xlsx'
+              'before proceeding')
+        exit()
+
+    data.dropna(how='all', inplace=True)
+    count_row = data.shape[0]
+
+    if f != b and f != g:
+        data = data.stack(head)
+
+    if f == c:
+        data.index.names = ['State', 'Year']
+        data.name = 'Divorces Per 1000'
+    elif f == d:
+        data.index.names = ['State', 'Year']
+        data.name = 'Marriages Per 1000'
+    # elif f == 'CrimeOneYearofData.csv':
+
+    data = data.reset_index()
+
+    if f == a:
+        data.rename(columns={'level_0': 'State',
+                             data.columns[1]: 'Year',
+                             'level_2': 'Income',
+                             0: 'USD$'},
+                    inplace=True)
+    elif f == e:
+        data.rename(columns={data.columns[0]: 'Years',
+                             data.columns[1]: 'Type of residence in the United States',
+                             data.columns[2]: 'Different or Same County',
+                             data.columns[3]: 'Different or Same State',
+                             data.columns[4]: 'total',
+                             }, inplace=True)
+
+    if count_row < 65536:
+        data.to_excel(excel_writer='data/cleaned/cleaned_{0}.xls'.format(f),
+                      sheet_name='cleaned',
+                      na_rep='null',
+                      index=False)
+        print('Cleaned file successfully saved in .xls format.')
+    else:
+        data.to_csv(path_or_buf='data/cleaned/cleaned_{0}.csv'.format(f),
+                    na_rep='null',
+                    index=False)
+        print('Notice: your file was too large to be converted to Excel format, and has'
+              ' successfully been saved as .csv')
 
 
-print('This program supports any of the following file names:\n',a, '\n',
-      b, '\n', c, '\n', d, '\n', e, '\n', g, '\n', h, '\n'
-      'Custom files may be supported by this program.')
+def main():
 
-filenames = [a, b, c, d, e, g, h]
+    while True:
+        filenames = [a, b, c, d, e, g, h]
 
-f = input('Please enter the name of the file that you want to use: ')
+        f = input('Please enter the name of the file that you want to use: ')
 
-if f not in filenames:
-    sr = input('How many lines need to be skipped in the header? ')
-    sf = input('How many lines need to be skipped in the footer? ')
-    na = input('How are null values represented in this dataset? ')
-    ic = [int(x) for x in input('Enter which column(s) will be used for the index'
-                                ' (separate with only spaces, NOT commas): ').split()]
-    h = [int(x) for x in input('Enter which column(s) will be used for the header'
-                               ' (separate with only spaces, NOT commas): ').split()]
-elif f == a:
-    sr = 4
-    h = [0, 1]
-    sf = 1
-    ic = 0
-    na = 'null'
-elif f == b or f == g:
-    sr = 6
-    na = 'N/A'
-    ic = 0
-    h = 0
-    sf = 0
-elif f == c:
-    sr = 5
-    na = '---'
-    sf = 7
-    ic = 0
-    h = 0
-elif f == d:
-    sr = 5
-    na = '---'
-    sf = 8
-    ic = 0
-    h = 0
-elif f == e:
-    sr = 4
-    na = '(NA)'
-    ic = 0
-    h = [0, 1, 2]
-    sf = 12
-elif f == h:
-    merger(f)
-    sr = 4
-    h = 0
-    sf = 1
-    ic = 0
-    na = 'null'
+        cleaner(f, filenames)
 
-if '.csv' in f:
-    data = csv(f, sr, sf, na, ic, h)
-elif '.xls' in f:
-    data = excel(f, sr, sf, na, ic, h)
-else:
-    print('Data in unsupported format. Please convert file to .csv or .xls/.xlsx'
-          'before proceeding')
-    exit()
+        while True:
+            answer = input('Would you like to clean another file? (y/n): ')
+            if answer in ('y', 'n', 'Y', 'N'):
+                break
+            else:
+                input('Invalid input. Would you like to run this program again? (y/n): ')
+                if answer in ('y', 'n', 'Y', 'N'):
+                    break
+                else:
+                    exit()
 
-data.dropna(how='all', inplace=True)
-count_row = data.shape[0]
-
-if f != b and f != g:
-    data = data.stack(h)
+        if answer == 'y' or answer == 'Y':
+            continue
+        else:
+            print('Thank you, goodbye.')
+            break
 
 
-if f == c:
-    data.index.names = ['State', 'Year']
-    data.name = 'Divorces Per 1000'
-elif f == d:
-    data.index.names = ['State', 'Year']
-    data.name = 'Marriages Per 1000'
-# elif f == 'CrimeOneYearofData.csv':
-
-data = data.reset_index()
-
-if f == a:
-    data.rename(columns={'level_0': 'State',
-                         data.columns[1]: 'Year',
-                         'level_2': 'Income',
-                         0: 'USD$'},
-                inplace=True)
-elif f == e:
-    data.rename(columns={data.columns[0]: 'Years',
-                         data.columns[1]: 'Type of residence in the United States',
-                         data.columns[2]: 'Different or Same County',
-                         data.columns[3]: 'Different or Same State',
-                         data.columns[4]: 'total',
-                         }, inplace=True)
-
-if count_row < 65536:
-    data.to_excel(excel_writer='data/cleaned/cleaned_{0}.xls'.format(f),
-                  sheet_name='cleaned_{0}'.format(f),
-                  na_rep='null',
-                  index=False)
-    print('Cleaned file successfully saved in .xls format.')
-else:
-    data.to_csv(path_or_buf='data/cleaned/cleaned_{0}.csv'.format(f),
-                na_rep='null',
-                index=False)
-    print('Notice: your file was too large to be converted to Excel format, and has'
-          ' successfully been saved as .csv')
-
+print('This program supports any of the following file names:\n', a, '\n',
+       b, '\n', c, '\n', d, '\n', e, '\n', g, '\n', h, '\n Custom files may'
+       ' be supported by this program.')
+main()
