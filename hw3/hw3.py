47c0a9a6d6abea381afb8a4510a07014d5faa265
Audrey Thoma <audrey-thoma@uiowa.edu>
2019-02-21 11:36:47 -0600

    adding filename strings as variables

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -31,15 +31,20 @@ def merger(f):
     merged.to_csv('data/{0}.csv'.format(f), index=False)
 
 
-print('This program supports any of the following file names:\nh08b.xls\n'
-      'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv\n'
-      'state-divorce-rates-90-95-99-17.xlsx\nstate-marriage-rates-90-95-99-17.xlsx\n'
-      'tab-a-1.xls\nUnemployment rate by state 2000-2017.csv\nCrimeTrendsInOneVar.csv\n'
+a = 'h08b.xls'
+b = 'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv'
+c = 'state-divorce-rates-90-95-99-17.xlsx'
+d = 'state-marriage-rates-90-95-99-17.xlsx'
+e = 'tab-a-1.xls'
+g = 'Unemployment rate by state 2000-2017.csv'
+h = 'CrimeTrendsInOneVar.csv'
+
+
+print('This program supports any of the following file names:\n',a, '\n',
+      b, '\n', c, '\n', d, '\n', e, '\n', g, '\n', h, '\n'
       'Custom files may be supported by this program.')
 
-filenames = ['h08b.xls', 'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv',
-             'state-divorce-rates-90-95-99-17.xlsx', 'state-marriage-rates-90-95-99-17.xlsx',
-             'tab-a-1.xls', 'Unemployment rate by state 2000-2017.csv']
+filenames = [a, b, c, d, e, g, h]
 
 f = input('Please enter the name of the file that you want to use: ')
 
@@ -51,43 +56,37 @@ if f not in filenames:
                                 ' (separate with only spaces, NOT commas): ').split()]
     h = [int(x) for x in input('Enter which column(s) will be used for the header'
                                ' (separate with only spaces, NOT commas): ').split()]
-elif f == 'state-divorce-rates-90-95-99-17.xlsx':
-    sr = 5
-    na = '---'
-    sf = 7
-    ic = 0
-    h = 0
-elif f == 'state-marriage-rates-90-95-99-17.xlsx':
-    sr = 5
-    na = '---'
-    sf = 8
-    ic = 0
-    h = 0
-elif f == 'Unemployment rate by state 2000-2017.csv':
-    sr = 6
-    na = 'N/A'
-    ic = 0
-    h = 0
-    sf = 0
-elif f == 'tab-a-1.xls':
-    sr = 4
-    na = '(NA)'
-    ic = 0
-    h = [0, 1, 2]
-    sf = 12
-elif f == 'h08b.xls':
+elif f == a:
     sr = 4
     h = [0, 1]
     sf = 1
     ic = 0
     na = 'null'
-elif f == 'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv':
+elif f == b or f == g:
     sr = 6
     na = 'N/A'
-    sf = 0
     ic = 0
     h = 0
-elif f == 'CrimeTrendsInOneVar.csv':
+    sf = 0
+elif f == c:
+    sr = 5
+    na = '---'
+    sf = 7
+    ic = 0
+    h = 0
+elif f == d:
+    sr = 5
+    na = '---'
+    sf = 8
+    ic = 0
+    h = 0
+elif f == e:
+    sr = 4
+    na = '(NA)'
+    ic = 0
+    h = [0, 1, 2]
+    sf = 12
+elif f == h:
     merger(f)
     sr = 4
     h = 0
@@ -107,34 +106,29 @@ else:
 data.dropna(how='all', inplace=True)
 count_row = data.shape[0]
 
-if f != 'Unemployment rate by state 2000-2017.csv' and \
-        f != 'Health Insurance Coverage Type by Family Income and Age 2008-2017.csv':
+if f != b and f != g:
     data = data.stack(h)
 
-if f == 'state-divorce-rates-90-95-99-17.xlsx':
+if f == a:
+    data.rename(columns={'level_0': 'State',
+                         data.columns[1]: 'Year',
+                         'level_2': 'Income',
+                         0: 'USD$'},
+                inplace=True)
+elif f == c:
     data.index.names = ['State', 'Year']
     data.name = 'Divorces Per 1000'
-elif f == 'state-marriage-rates-90-95-99-17.xlsx':
+elif f == d:
     data.index.names = ['State', 'Year']
     data.name = 'Marriages Per 1000'
-elif f == 'tab-a-1.xls':
+elif f == e:
     data.rename(columns={data.columns[0]: 'Years',
                          data.columns[1]: 'Type of residence in the United States',
                          data.columns[2]: 'Different or Same County',
                          data.columns[3]: 'Different or Same State',
                          data.columns[4]: 'total',
                          }, inplace=True)
-elif f == 'h08b.xls':
-    data.rename(columns={'level_0': 'State',
-                         data.columns[1]: 'Year',
-                         'level_2': 'Income',
-                         0: 'USD$'},
-                inplace=True)
 # elif f == 'CrimeOneYearofData.csv':
-        #    sr = 5
-        #    na = "---"
-        #    sf = 7
-        #    index_col = 0
 
 data = data.reset_index()
 
@@ -143,8 +137,11 @@ if count_row < 65536:
                   sheet_name='cleaned_{0}'.format(f),
                   na_rep='null',
                   index=False)
+    print('Cleaned file successfully saved in .xls format.')
 else:
     data.to_csv(path_or_buf='data/cleaned/cleaned_{0}.csv'.format(f),
                 na_rep='null',
                 index=False)
+    print('Notice: your file was too large to be converted to Excel format, and has'
+          ' successfully been saved as .csv')
 
